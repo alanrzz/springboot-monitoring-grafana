7ed1c208a7453150f4d22d34ab8351b8dbbc9ee1
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2022-10-04 19:49:25 -0300

    initial commit

---
+++ src/main/java/org/springboot/monitoring/api/SpringbootMonitoringGrafanaApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.monitoring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMonitoringGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMonitoringGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/springboot/monitoring/api/SpringbootMonitoringGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.monitoring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMonitoringGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
