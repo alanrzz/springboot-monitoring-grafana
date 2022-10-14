31f13427225e352e596958a3727ac9518757a80f
Alan Rodriguez <alanrodriguezalexandro@gmail.com>
2022-10-14 14:45:08 -0300

    fix actuator - springfox

--- src/main/java/org/springboot/monitoring/api/config/SwaggerConfig.java
+++ src/main/java/org/springboot/monitoring/api/config/SwaggerConfig.java
@@ -1,8 +1,22 @@
 package org.springboot.monitoring.api.config;
 
 import org.springboot.monitoring.api.controllers.TodoController;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
+import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -10,6 +24,10 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -33,5 +51,22 @@ public class SwaggerConfig {
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .contact(new Contact("Alan Rodriguez", "https://github.com/alanrzz", "alanrodriguezalexandro@gmail.com"))
                 .build();
+    }
+
+    @Bean
+    public WebMvcEndpointHandlerMapping handler(WebEndpointsSupplier w0, ServletEndpointsSupplier s, ControllerEndpointsSupplier c0, EndpointMediaTypes e, CorsEndpointProperties c1, WebEndpointProperties w1, Environment env) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = w0.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(s.getEndpoints());
+        allEndpoints.addAll(c0.getEndpoints());
+        String basePath = w1.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(w1, env, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, e, c1.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath), shouldRegisterLinksMapping, null);
+    }
+
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }
